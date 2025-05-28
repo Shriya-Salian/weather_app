5838f29cb5d2f4707b9fbd1ce3e72b4ef3c1df07
Shriya-Salian <salianshriya21@gmail.com>
2025-05-29 00:25:56 +0530

    initial setup

---
+++ src/components/Input.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Input = (className="", ...props)  =>{
+  return (
+    <input type="text" 
+    className={``}
+    
+    
+    
+    
+    
+    
+    />
+  )
+}
+
+export default Input
